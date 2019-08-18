aa3ce71ef556beac7d4e18c16880fb0cdfa2c205
Daniel Wagner <me@dmwit.com>
2019-08-18 18:48:35 -0400

    slightly cooler demo

--- bin/demo.hs
+++ bin/demo.hs
@@ -1,9 +1,16 @@
-import Dr.Mario.Model hiding (pp)
-import Dr.Mario.Sveta
+import System.Random.MWC
+import qualified Dr.Mario.Model as M
+import qualified Dr.Mario.Sveta as S
 
 main :: IO ()
-main = putStr . pp $ reachable
-	(emptyBoard 8 16)
-	14
-	(Pill (PillContent Horizontal Red Blue) (Position 3 15))
-	Checking
+main = do
+	gen <- createSystemRandom
+	board <- flip M.randomBoard 4 <$> uniformR (1, maxBound) gen
+	c1 <- M.decodeColor <$> uniformR (1, maxBound) gen
+	c2 <- M.decodeColor <$> uniformR (1, maxBound) gen
+	putStr . M.pp $ board
+	putStr . S.pp $ S.reachable
+		board
+		14
+		(M.Pill (M.PillContent M.Horizontal c1 c2) (M.Position 3 15))
+		S.Checking
