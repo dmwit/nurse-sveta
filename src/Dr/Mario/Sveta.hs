bc0661d161cfcfb0beadb161f798b895fdc43e9e
Daniel Wagner <me@dmwit.com>
2019-08-23 15:11:21 -0400

    hook up most of the domain knowledge needed

--- src/Dr/Mario/Sveta.hs
+++ src/Dr/Mario/Sveta.hs
@@ -1,4 +1,180 @@
 module Dr.Mario.Sveta where
 
+import Data.IORef
+import Data.List
+import Data.Ord
+import Data.Vector (Vector)
+import Dr.Mario.Model
 import Dr.Mario.Sveta.MCTS
 import Dr.Mario.Sveta.Pathfinding
+import Dr.Mario.Util
+import System.Random.MWC
+import System.Random.MWC.CondensedTable
+import qualified Dr.Mario.Model as M
+import qualified Dr.Mario.Sveta.Pathfinding as S
+import qualified Data.Vector as V
+
+data MCStats = MCStats
+	{ visitCount :: !Double
+	, cumulativeUtility :: !Double
+	} deriving (Eq, Ord, Read, Show)
+
+type MCScore = Down Double
+
+data MCMove
+	= AIMove Pill
+	| ChanceMove Color Color
+	deriving (Eq, Ord, Read, Show)
+
+type MCM = IO
+
+data MCPosition = MCPosition
+	{ mboard :: IOBoard
+	, auxState :: IORef AuxiliaryState
+	, originalVirusCount :: Double
+	}
+
+data AuxiliaryState = AuxiliaryState
+	{ pillsUsed :: !Double
+	, pillsUsedSinceVirusClear :: !Double
+	, lookahead :: Maybe (Color, Color)
+	, virusesCleared :: !Int
+	} deriving (Eq, Ord, Read, Show)
+
+data MCPlayer = AI | Chance deriving (Bounded, Enum, Eq, Ord, Read, Show)
+
+type DrMarioTree = MCTree MCStats MCScore MCMove
+type DrMarioParameters = MCTSParameters MCM MCStats MCScore MCMove MCPosition MCPlayer
+
+stallThreshold :: Double
+stallThreshold = 20
+
+mtoppedOut :: MCPosition -> IO Bool
+mtoppedOut mcpos = check startingBottomLeftPosition `orM` check startingOtherPosition where
+	check :: Position -> IO Bool
+	check p = (Just Empty /=) <$> mget (mboard mcpos) p
+
+timedOut :: AuxiliaryState -> Bool
+timedOut aux = pillsUsedSinceVirusClear aux > stallThreshold
+
+mtimedOut :: MCPosition -> IO Bool
+mtimedOut mcpos = timedOut <$> readIORef (auxState mcpos)
+
+mlost :: MCPosition -> IO Bool
+mlost mcpos = orM (mtimedOut mcpos) (mtoppedOut mcpos)
+
+won :: MCPosition -> AuxiliaryState -> Bool
+won mcpos aux = originalVirusCount mcpos == fromIntegral (virusesCleared aux)
+
+mwon :: MCPosition -> IO Bool
+mwon mcpos = won mcpos <$> readIORef (auxState mcpos)
+
+dmScore :: MCPlayer -> MCStats -> MCStats -> MCScore
+dmScore AI statsParent statsCurrent = ucb1 (visitCount statsParent) (visitCount statsCurrent) (cumulativeUtility statsCurrent)
+dmScore Chance _ statsCurrent = Down (visitCount statsCurrent)
+
+dmEvaluate :: MCPosition -> MCM MCStats
+dmEvaluate mcpos = do
+	aux <- readIORef (auxState mcpos)
+	longerIsBetter <- mtoppedOut mcpos
+	let x &&& y | longerIsBetter = x+y
+	            | otherwise      = x+1-y
+	    pu = pillsUsed aux
+	    clearUtility = fromIntegral (virusesCleared aux)
+	    usageUtility = pu / (pu + 20)
+	    maxUtility = originalVirusCount mcpos + 1
+	pure MCStats
+		{ visitCount = 1
+		, cumulativeUtility = (clearUtility &&& usageUtility) / maxUtility
+		}
+
+allChanceMoves :: Vector Move
+allChanceMoves = V.fromListN 6
+	[ ChanceMove l r
+	| l:rest <- tails [minBound .. maxBound]
+	, r <- rest
+	]
+
+dmExpand :: MCPosition -> MCM (Vector MCMove)
+dmExpand mcpos = do
+	aux <- readIORef (auxState mcpos)
+	done <- orM
+		(return (timedOut aux || won mcpos aux))
+		(mtoppedOut mcpos)
+	if done then pure V.empty else case lookahead aux of
+		-- TODO: return all pills, but share rollouts between neighboring
+		-- flipped moves when they both exist
+		--
+		-- ...or maybe that would get too expensive, because then there's 2^n
+		-- paths to update when we're processing an MCTree node n levels deep?
+		-- but then what to do about moves that can't be rotated 180? hmmm...
+		Nothing -> pure allChanceMoves
+		Just (l, r) -> munsafeApproxReachable (mboard mcpos) (launchPill l r)
+
+dmRoot :: Board -> MCM MCPosition
+dmRoot b = do
+	mb <- thaw b
+	auxRef <- newIORef AuxiliaryState
+		{ pillsUsed = 0
+		, pillsUsedSinceVirusClear = 0
+		, lookahead = Nothing
+		, virusesCleared = 0
+		}
+	pure MCPosition
+		{ mboard = mb
+		, auxState = auxRef
+		, originalVirusCount = fromIntegral viruses
+		}
+	where
+	viruses = length
+		[ ()
+		| x <- [0..7]
+		, y <- [0..15]
+		, Just (Occupied _ Virus) <- [get b (Position x y)]
+		]
+
+dmTurn :: MCPosition -> MCM MCPlayer
+dmTurn mcpos = maybe Chance (const AI) . lookahead <$> readIORef (auxState mcpos)
+
+dmPlay :: MCPosition -> MCMove -> MCM ()
+dmPlay mcpos (ChanceMove l r) = modifyIORef (auxState mcpos) (\aux -> aux { lookahead = Just (l, r) })
+dmPlay mcpos (AIMove p) = do
+	mVirusCount <- mplace (mboard mcpos) p
+	case mVirusCount of
+		Just virusCount -> modifyIORef (auxState mcpos) $ \aux -> aux
+			{ pillsUsed = pillsUsed aux + 1
+			, pillsUsedSinceVirusClear = if virusCount > 0 then 0 else pillsUsedSinceVirusClear aux + 1
+			, lookahead = Nothing
+			, virusesCleared = virusesCleared aux + virusCount
+			}
+		Nothing -> do
+			b <- mfreeze (mboard mcpos)
+			error
+				$ "The impossible happened: the AI chose an illegal pill placement of "
+				++ S.pp p ++ " on this board:\n"
+				++ M.pp b
+
+chanceMovesTable :: CondensedTableV MCMove
+chanceMovesTable = tableFromProbabilities . V.fromListN 6 $
+	[ (ChanceMove l r, if l == r then 1/9 else 2/9)
+	| l:rest <- tails [minBound .. maxBound]
+	, r <- rest
+	]
+
+dmSelect :: GenIO -> MCPosition -> Vector MCMove -> MCM MCMove
+dmSelect gen _ ms = case V.head ms of
+	ChanceMove _ _ -> genFromTable chanceMovesTable gen
+	_ -> (ms `V.unsafeIndex`) <$> uniformR (0, V.length ms-1) gen
+
+dmParameters :: GenIO -> Board -> DrMarioParameters
+dmParameters gen b = MCTSParameters
+	{ score = dmScore
+	, evaluate = dmEvaluate
+	, expand = dmExpand
+	, root = dmRoot b
+	, turn = dmTurn
+	, play = dmPlay
+	, select = dmSelect gen
+	}
+
+munsafeApproxReachable = undefined
--- src/Dr/Mario/Sveta/MCTS.hs
+++ src/Dr/Mario/Sveta/MCTS.hs
@@ -1,11 +1,24 @@
-module Dr.Mario.Sveta.MCTS where
+module Dr.Mario.Sveta.MCTS (
+	MCTree(..),
+	MCTSParameters(..),
+	emptyTree,
+	mcts,
+	ucb1,
+	-- * Convenience re-exports
+	Down(..),
+	NonEmpty(..)
+	) where
 
 import Data.Hashable
 import Data.HashPSQ (HashPSQ)
 import Data.List.NonEmpty (NonEmpty((:|)))
+import Data.Ord
+import Data.Vector (Vector)
 import qualified Data.HashPSQ as Q
 import qualified Data.List.NonEmpty as NE
+import qualified Data.Vector as V
 
+-- | A representation of the current state of a run of Monte Carlo tree search.
 data MCTree stats score move = MCTree
 	{ statistics :: stats
 	, children :: HashPSQ move score (MCTree stats score move)
@@ -13,15 +26,42 @@ data MCTree stats score move = MCTree
 	} deriving (Eq, Show)
 
 data MCTSParameters m stats score move position player = MCTSParameters
-	{ score :: player -> stats -> score
+	{ score :: player -> stats -> stats -> score
+	-- ^ Given which player is currently choosing a move and the statistics for
+	-- the parent node and current node, compute a priority score for searching
+	-- this node in the future. Smaller scores are searched first.
+	--
+	-- See also 'ucb1'.
 	, evaluate :: position -> m stats
-	, expand :: position -> m [move]
+	-- ^ Given a leaf position, compute some statistics. These statistics
+	-- typically include the utilities for each player and a count of times
+	-- visited (which this function should initialize to 1). The statistics
+	-- will be backpropogated up the tree to the root using their 'Semigroup'
+	-- instance.
+	--
+	-- You may assume this function is only invoked in a state in which
+	-- 'expand' would produce the empty list.
+	, expand :: position -> m (Vector move)
+	-- ^ Compute the moves available from this position. Return an empty vector
+	-- to indicate that this is a leaf node (i.e. a won or lost position).
 	, root :: m position
+	-- ^ An action which can be used to regenerate a fresh, mutable copy of the
+	-- game state you want to choose a move in.
 	, turn :: position -> m player
+	-- ^ Compute which player is currently making choices.
 	, play :: position -> move -> m ()
-	, select :: position -> NonEmpty move -> m move
+	-- ^ Mutate the current position, making the given move.
+	, select :: position -> Vector move -> m move
+	-- ^ Randomly select one of the provided moves. Typically this will choose
+	-- uniformly at random from the provided collection of moves, but a handle
+	-- to the current position is provided in case you'd like to run a deep
+	-- neural net to compute move priors or something crazy like that.
+	--
+	-- You may assume this function is only invoked with non-empty vectors
+	-- returned from 'expand'.
 	}
 
+-- | Create an essentially empty tree, suitable for providing to 'mcts'.
 emptyTree ::
 	(Monad m, Monoid stats) =>
 	MCTSParameters m stats score move position player ->
@@ -32,9 +72,12 @@ emptyTree params = do
 	pure MCTree
 		{ statistics = mempty
 		, children = Q.empty
-		, unexplored = ms
+		, unexplored = V.toList ms
 		}
 
+-- | Perform one iteration of Monte Carlo tree search (called a rollout in the
+-- literature). You should iterate this until you run out of computational
+-- budget.
 mcts ::
 	(Monad m, Semigroup stats, Hashable move, Ord move, Ord score) =>
 	MCTSParameters m stats score move position player ->
@@ -58,9 +101,10 @@ mcts_ params pos = go where
 				player <- turn params pos
 				play params pos m
 				(stats, t'') <- go t'
+				let stats' = statistics t <> stats
 				pure $ (stats, t
-					{ statistics = statistics t <> stats
-					, children = Q.insert m (score params player (statistics t'')) t'' q
+					{ statistics = stats'
+					, children = Q.insert m (score params player stats' (statistics t'')) t'' q
 					})
 			Nothing -> do
 				stats <- evaluate params pos
@@ -70,13 +114,24 @@ mcts_ params pos = go where
 			play params pos m
 			ms' <- expand params pos
 			stats <- rollout ms'
+			let stats' = statistics t <> stats
 			pure (stats, t
-				{ statistics = statistics t <> stats
-				, children = Q.insert m (score params player stats) (MCTree stats Q.empty ms') (children t)
+				{ statistics = stats'
+				, children = Q.insert m (score params player stats' stats) (MCTree stats Q.empty (V.toList ms')) (children t)
 				, unexplored = ms
 				})
 
-	rollout [] = evaluate params pos
-	rollout (m:ms) = do
-		select params pos (m:|ms) >>= play params pos
+	rollout ms | V.null ms = evaluate params pos
+	rollout ms = do
+		select params pos ms >>= play params pos
 		expand params pos >>= rollout
+
+-- | Compute the popular upper-confidence bound score.
+--
+-- Arguments are a visit count for the parent node (n in the literature), a
+-- visit count for the current node (n_i in the literature), and a cumulative
+-- utility achieved by children of the current node (Q_i in the literature).
+-- The utility of individual leaves should be in the range [0, 1] (so that 0 <=
+-- Q_i <= n_i).
+ucb1 :: Double -> Double -> Double -> Down Double
+ucb1 n n_i q_i = Down $ q_i/n_i + sqrt (2 * log n / n_i)
