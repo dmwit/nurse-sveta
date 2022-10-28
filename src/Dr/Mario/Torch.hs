75095f679e5868d2b58a858f129eeed73699e4a8
Daniel Wagner <dmwit@galois.com>
2022-10-28 19:59:20 -0400

    enough stuff that we could start using a neural net in place of dumbEvaluation... I think

--- src/Dr/Mario/Torch.hs
+++ src/Dr/Mario/Torch.hs
@@ -1,15 +1,16 @@
+{-# LANGUAGE AllowAmbiguousTypes #-}
 {-# Language DataKinds #-}
 
 module Dr.Mario.Torch (
-	NurseSvetaNetDef, netDefForward,
-	NurseSvetaNetSpec(..), NurseSvetaNet, netForward,
-	NetInput(..),
-	NetOutput(..),
+	NurseSvetaNetDef, netDefForwardSS, netDefForwardST, netDefForwardTT,
+	NurseSvetaNetSpec(..), nurseSvetaNetSpec, nurseSvetaNetSpecDef, NurseSvetaNet, netForward,
+	NetInputStruct(..), NetInputTensor(..), nisToTensor,
+	NetOutputStruct(..), NetOutputTensor(..), notToStruct,
 	ResidualSpec(..), Residual, residualForward,
-	StableSquareConvSpec(..), ConstSquareConvSpec, constChansSpec,
+	StableSquareConvSpec(..), ConstSquareConvSpec, stableSpec, constChansSpec,
 	StableSquareConv, ConstSquareConv,
 	Conv2dReLUInitSpec(..), Conv2dReLUInit,
-	BatchNorm, bnNew, bnForward,
+	BatchNormSpec(..), BatchNorm, bnForward,
 	unsafeAsTensor,
 	KnownConfig, KnownAndValidRand, BatchNormDTypeIsValid, CPU0
 	) where
@@ -27,7 +28,9 @@ import Dr.Mario.Model.Internal
 import GHC.Exts
 import GHC.TypeLits
 import Torch.Typed hiding (sqrt)
+import Unsafe.Coerce
 
+import qualified Data.HashMap.Strict as HM
 import qualified Data.Vector as VU
 import qualified Data.Vector.Unboxed.Mutable as MVU
 import qualified Data.Vector.Sized as VS
@@ -78,11 +81,15 @@ instance KnownAndValidRand [iChans, oChans, kw, kh] dtype dev => Randomizable
 		numParameters = fromIntegral $ natValI @kw * natValI @kh * natValI @iChans
 
 type ConstSquareConvSpec chans = StableSquareConvSpec chans chans
-newtype StableSquareConvSpec (iChans :: Nat) (oChans :: Nat) (p :: Nat) (dtype :: DType) (dev :: (DeviceType, Nat)) = StableSquareConvSpec { stableSquareLeakage :: Double }
+newtype StableSquareConvSpec iChans oChans p dtype dev = StableSquareConvSpec
+	{ stableSquareLeakage :: Conv2dReLUInitSpec iChans oChans (2*p+1) (2*p+1) dtype dev }
 	deriving (Eq, Ord, Read, Show)
 
-constChansSpec :: Double -> ConstSquareConvSpec chans k dtype dev
-constChansSpec = StableSquareConvSpec
+stableSpec :: Double -> StableSquareConvSpec iChans oChans p dtype dev
+stableSpec = StableSquareConvSpec . Conv2dReLUInitSpec
+
+constChansSpec :: Double -> ConstSquareConvSpec chans p dtype dev
+constChansSpec = stableSpec
 
 -- | Like 'Conv2dReLUInit', except for two things:
 --
@@ -99,7 +106,7 @@ instance KnownAndValidRand [iChans, oChans, 2*p+1] dtype dev => Randomizable
 	(StableSquareConvSpec iChans oChans p dtype dev)
 	(StableSquareConv     iChans oChans p dtype dev)
 	where
-	sample (StableSquareConvSpec n) = StableSquareConv <$> sample (Conv2dReLUInitSpec n)
+	sample (StableSquareConvSpec n) = StableSquareConv <$> sample n
 
 instance (k ~ (2*p+1)
 	, All KnownNat [iChans, oChans, k, w, h, n, p]
@@ -112,6 +119,20 @@ instance (k ~ (2*p+1)
 	where
 	forward (StableSquareConv c) t = forward c (t, Proxy @'(1,1), Proxy @'(p,p))
 	forwardStoch (StableSquareConv c) t = forwardStoch c (t, Proxy @'(1,1), Proxy @'(p,p))
+
+data BatchNormSpec c dtype dev = BatchNormSpec
+	{ bnsMomentum :: Double -- ^ the sensible kind, as in most optimizers -- e.g. 0.9ish is sensible
+	, bnsEpsilon :: Double
+	} deriving (Eq, Ord, Read, Show)
+
+instance KnownConfig '[c] dtype dev => Randomizable (BatchNormSpec c dtype dev) (BatchNorm c dtype dev) where
+	sample spec = pure BatchNorm
+		<*> makeIndependent ones
+		<*> makeIndependent zeros
+		<*> newIORef zeros
+		<*> newIORef zeros
+		<*> pure (expand False (full @'[] (bnsMomentum spec)))
+		<*> pure (expand False (full @'[] (bnsEpsilon spec)))
 
 -- The BatchNorm that ships with hasktorch is just broken. See
 -- https://github.com/hasktorch/hasktorch/issues/626. Also it doesn't even
@@ -134,20 +155,6 @@ instance Parameterized (BatchNorm c dtype dev) where
 	type Parameters (BatchNorm c dtype dev) = [Parameter dev dtype '[c], Parameter dev dtype '[c]]
 	flattenParameters bn = bnWeight bn ::: bnBias bn ::: HNil
 	replaceParameters bn (w ::: b ::: HNil) = bn { bnWeight = w, bnBias = b }
-
-bnNew :: KnownConfig '[c] dtype dev =>
-	-- | momentum (the sensible kind, like all the other NN layers -- e.g. 0.9ish is sensible)
-	Double ->
-	-- | epsilon
-	Double ->
-	IO (BatchNorm c dtype dev)
-bnNew momentum epsilon = pure BatchNorm
-	<*> makeIndependent ones
-	<*> makeIndependent zeros
-	<*> newIORef zeros
-	<*> newIORef zeros
-	<*> pure (expand False (full @'[] momentum))
-	<*> pure (expand False (full @'[] epsilon))
 
 type BatchNormDTypeIsValid dev dtype =
 	( KnownDevice dev
@@ -198,14 +205,27 @@ bnAbsorb ::
 	Tensor dev dtype '[c] -> Tensor dev dtype '[c] -> Tensor dev dtype '[c]
 bnAbsorb bn new old = bnMomentum bn * old + (1-bnMomentum bn) * new
 
-newtype ResidualSpec chans p dtype dev = ResidualSpec { residualSpecLeakage :: Double }
-	deriving (Eq, Ord, Read, Show)
+data ResidualSpec chans p dtype dev = ResidualSpec
+	{ rsLeakage :: Double
+	, rsBatchNormSpec :: BatchNormSpec chans dtype dev
+	} deriving (Eq, Ord, Read, Show)
 
 data Residual chans p dtype dev = Residual
 	{ resLeakage :: Double
 	, resConv0, resConv1 :: ConstSquareConv chans p dtype dev
 	, resNorm0, resNorm1 :: BatchNorm chans dtype dev
 	}
+
+instance KnownAndValidRand [chans, 2*p+1] dtype dev => Randomizable
+	(ResidualSpec chans p dtype dev)
+	(Residual     chans p dtype dev)
+	where
+	sample ResidualSpec { rsLeakage = leakage, rsBatchNormSpec = bns } = pure Residual
+		<*> pure leakage
+		<*> sample (constChansSpec leakage)
+		<*> sample (constChansSpec leakage)
+		<*> sample bns
+		<*> sample bns
 
 residualForward :: (k ~ (2*p+1)
 	, BatchNormDTypeIsValid dev dtype
@@ -228,11 +248,21 @@ residualForward res training t = do
 
 -- TODO: ideas for other inputs: NES/MWC pill RNG, cost model stats, gravity,
 -- NES vs SNES movement, NES vs SNES pill distribution
-data NetInput = NetInput
-	{ niBoard :: IOBoard
-	, niLookaheadL :: Color
-	, niLookaheadR :: Color
+data NetInputStruct = NetInputStruct
+	{ nisBoard :: IOBoard
+	, nisLookaheadL :: Color
+	, nisLookaheadR :: Color
 	}
+
+data NetInputTensor dev where
+	NetInputTensor ::
+		( KnownNat n
+		, KnownNat (PerCell n)
+		, 1 <= PerCell n
+		) =>
+		{ nitBoard :: Tensor dev 'Double [n, 7, 8, 16]
+		, nitLookahead :: Tensor dev 'Double [n, 6]
+		} -> NetInputTensor dev
 
 class OneHot a where
 	toIndex :: a -> Int
@@ -257,36 +287,38 @@ instance OneHot DM.Shape where
 -- It would be really bad to share the return value of zeros across calls to
 -- this function, since we modify it in-place. Hopefully this NoInline pragma
 -- will prevent that.
-{-# NoInline niToTensor #-}
-niToTensor :: forall dev n. (KnownDevice dev, KnownNat n) =>
-	[NetInput] -> IO (Tensor dev 'Double [n, 7, 8, 16], Tensor dev 'Double '[n, 6])
-niToTensor nis = do
-	assert (n == natValI @n) $ "expected " ++ show (natValI @n) ++ " net inputs, but saw " ++ show n
+{-# NoInline nisToTensor #-}
+nisToTensor :: forall dev. KnownDevice dev => [NetInputStruct] -> IO (NetInputTensor dev)
+nisToTensor nis = do
 	tBoard <- evaluate (TU.zeros [n, 7, 8, 16] opts)
 	tLookahead <- evaluate (TU.zeros [n, 6] opts)
 	TU.withTensor tBoard $ \pBoard ->
 		TU.withTensor tLookahead $ \pLookahead ->
 			for_ (zip [0..] nis) $ \(i, ni) -> do
-				assert (mwidth (niBoard ni) == 8 && mheight (niBoard ni) == 16)
-					$ "expected all boards to have size 8x16, but saw a board with size " ++ show (mwidth (niBoard ni)) ++ "x" ++ show (mheight (niBoard ni))
+				assert (mwidth (nisBoard ni) == 8 && mheight (nisBoard ni) == 16)
+					$ "expected all boards to have size 8x16, but saw a board with size " ++ show (mwidth (nisBoard ni)) ++ "x" ++ show (mheight (nisBoard ni))
 				let iBoard = i*onehotCount
 				    iLookahead = i*6
-				pokeOne pLookahead (    iLookahead + toIndex (niLookaheadL ni))
-				pokeOne pLookahead (3 + iLookahead + toIndex (niLookaheadR ni))
+				pokeOne pLookahead (    iLookahead + toIndex (nisLookaheadL ni))
+				pokeOne pLookahead (3 + iLookahead + toIndex (nisLookaheadR ni))
 				-- IOBoard stores cells in a 1D array with the y coordinate
 				-- varying fastest... just like the tensor we want to make.
 				-- This means we get to reuse the index into that array as an
 				-- index into our tensor. Nice.
-				MVU.iforM_ (mcells (niBoard ni)) $ \j cell -> case cell of
+				MVU.iforM_ (mcells (nisBoard ni)) $ \j cell -> case cell of
 					Empty -> pure ()
 					Occupied c s -> do
 						-- shiftl x 7 = x * cellCount
 						pokeOne pBoard (iBoard + shiftL (    toIndex c) 7 + j)
 						pokeOne pBoard (iBoard + shiftL (3 + toIndex s) 7 + j)
-	pure (UnsafeMkTensor tBoard, UnsafeMkTensor tLookahead)
+	case someNatVal (toInteger n) of
+		Just (SomeNat (_ :: Proxy n)) -> withDict (knownMul @n @128) $ case leNat @1 @(PerCell n) of
+			Just Dict -> pure $ NetInputTensor @n (UnsafeMkTensor tBoard) (UnsafeMkTensor tLookahead)
+			Nothing -> fail "Invalid arguments to nisToTensor: input list must be non-empty"
+		Nothing -> fail "The impossible happened in nisToTensor: length returned a negative number, or someNatVal failed on a non-negative number"
 	where
 	pokeOne p i = TU._pokeElemOff p i (1 :: Double)
-	assert b msg = unless b . fail $ "Assertion failure in niToTensor: " ++ msg
+	assert b msg = unless b . fail $ "Assertion failure in nisToTensor: " ++ msg
 
 	onehotCount, cellCount :: Int
 	onehotCount = 7*cellCount
@@ -303,16 +335,81 @@ niToTensor nis = do
 -- each kind of pill go? will each position be empty at end of game? maybe
 -- something about what order the viruses are cleared in, or what frame it
 -- happens on?
-data NetOutput = NetOutput
-	{ noPriors :: HashMap PillContent (VU.Vector (VU.Vector Double))
-	, noValuation :: Double
-	, noVirusClears :: Double
-	, noPills :: Double
-	, noFrames :: Double
+data NetOutputStruct = NetOutputStruct
+	-- TODO: maybe just use Tensors directly instead of vectors here?
+	{ nosPriors :: HashMap PillContent (VU.Vector (VU.Vector Double))
+	, nosValuation :: Double
+	, nosVirusClears :: Double
+	, nosPills :: Double
+	, nosFrames :: Double
 	}
 
-newtype NurseSvetaNetSpec depth filters p dev = NurseSvetaNetSpec { nsLeakage :: Double }
-	deriving (Eq, Ord, Read, Show)
+data NetOutputTensor dev where
+	NetOutputTensor :: KnownNat n =>
+		{ notPriors :: Tensor dev 'Double [n, 4, 8, 16]
+		, notValuation :: Tensor dev 'Double '[n]
+		, notScalars :: Tensor dev 'Double [n, 3]
+		} -> NetOutputTensor dev
+
+notToStruct :: [(Color, Color)] -> NetOutputTensor dev -> [NetOutputStruct]
+notToStruct lookaheads NetOutputTensor { notPriors = priors, notValuation = valuation, notScalars = scalars } =
+	[ NetOutputStruct
+		{ nosPriors = HM.fromList
+			[ ( pc
+			  , VS.fromSized . VS.generate @8 $ \x ->
+			    	VS.fromSized . VS.generate @16 $ \y ->
+			    		toDouble (selectIdx @0 (selectIdx @0 (selectIdx @0 tPriors numRots) x) y)
+			  )
+			| (numRots, pc) <- zip
+				[minBound .. maxBound]
+				(iterate (`rotateContent` Clockwise) (PillContent Horizontal l r))
+			]
+		, nosValuation = toDouble (selectIdx @0 valuation i)
+		, nosVirusClears = toDouble (select @0 @0 tScalars)
+		, nosPills = toDouble (select @0 @1 tScalars)
+		, nosFrames = toDouble (select @0 @2 tScalars)
+		}
+	| (i, (l, r)) <- zip [minBound .. maxBound] lookaheads
+	, let tPriors = selectIdx @0 priors i
+	      tScalars = selectIdx @0 scalars i
+	]
+
+data NurseSvetaNetSpec depth filters p dev = NurseSvetaNetSpec
+	{ nsBoardConvolutionSpec :: StableSquareConvSpec 7 filters p 'Double dev
+	, nsInputNormalizationSpec :: BatchNormSpec filters 'Double dev
+	, nsResidualsSpec :: ResidualSpec filters p 'Double dev
+	, nsPriorsConvolutionSpec :: StableSquareConvSpec filters 4 p 'Double dev
+	} deriving (Eq, Ord, Read, Show)
+
+nurseSvetaNetSpec :: Double -> BatchNormSpec filters 'Double dev -> NurseSvetaNetSpec depth filters p dev
+nurseSvetaNetSpec leakage bns = NurseSvetaNetSpec
+	{ nsBoardConvolutionSpec = stableSpec leakage
+	, nsInputNormalizationSpec = bns
+	, nsResidualsSpec = ResidualSpec
+		{ rsLeakage = leakage
+		, rsBatchNormSpec = bns
+		}
+	, nsPriorsConvolutionSpec = stableSpec leakage
+	}
+
+nurseSvetaNetSpecDef :: NurseSvetaNetSpecDef dev
+nurseSvetaNetSpecDef = nurseSvetaNetSpec 0.01 BatchNormSpec
+	{ bnsMomentum = 0.95
+	, bnsEpsilon = 1e-5
+	}
+
+instance KnownAndValidRand [depth, filters, PerCell filters, 2*p+1] 'Double dev => Randomizable
+	(NurseSvetaNetSpec depth filters p dev)
+	(NurseSvetaNet     depth filters p dev)
+	where
+	sample ns = pure NurseSvetaNet
+		<*> sample (nsBoardConvolutionSpec ns)
+		<*> sample LinearSpec
+		<*> sample (nsInputNormalizationSpec ns)
+		<*> VS.replicateM (sample (nsResidualsSpec ns))
+		<*> sample LinearSpec
+		<*> sample LinearSpec
+		<*> sample (nsPriorsConvolutionSpec ns)
 
 type PerCell n = Numel [n,8,16]
 
@@ -321,43 +418,86 @@ data NurseSvetaNet depth filters p dev = NurseSvetaNet
 	, netInputLinear :: Linear 6 (PerCell filters) 'Double dev
 	, netInputNormalization :: BatchNorm filters 'Double dev
 	, netResiduals :: VS.Vector depth (Residual filters p 'Double dev)
-	, netOutputLinear :: Linear (PerCell filters) 4 'Double dev
+	, netOutputFraction :: Linear (PerCell filters) 1 'Double dev
+	, netOutputPositive :: Linear (PerCell filters) 3 'Double dev
 	, netPriorsConvolution :: StableSquareConv filters 4 p 'Double dev
 	}
 
-netForward :: forall n depth filters p dev k.
+netForward :: forall depth filters p dev k.
 	( k ~ (2*p+1)
 	, BatchNormDTypeIsValid dev 'Double
-	, All KnownNat [filters, p, k, n, PerCell n, PerCell filters]
-	, AllDimsPositive [PerCell n, filters]
+	, All KnownNat [filters, p, k, PerCell filters]
+	, 1 <= filters
 	, ConvSideCheck  8 k 1 p  8
 	, ConvSideCheck 16 k 1 p 16
 	) =>
 	NurseSvetaNet depth filters p dev ->
 	Bool ->
-	Tensor dev 'Double [n, 7, 8, 16] ->
-	Tensor dev 'Double [n, 6] ->
-	IO (Tensor dev 'Double [n, 4, 8, 16], Tensor dev 'Double [n, 4])
-netForward net training board scalars = do
+	NetInputTensor dev ->
+	IO (NetOutputTensor dev)
+netForward net training NetInputTensor { nitBoard = board, nitLookahead = lookahead } = do
 	let a = forward (netBoardConvolution net) board
-	    b = forward (netInputLinear net) scalars
+	    b = forward (netInputLinear net) lookahead
 	    c = a + reshape b
 	d <- bnForward (netInputNormalization net) training c
 	e <- VS.foldM (\tensor layer -> residualForward layer training tensor) d (netResiduals net)
 	let f = forward (netPriorsConvolution net) e
-	    g = forward (netOutputLinear net) (reshape @[n, PerCell filters] e)
-	pure (f, g)
+	    g = reshape @[_, PerCell filters] e
+	    h = T.sigmoid $ forward (netOutputFraction net) g
+	    i = T.exp     $ forward (netOutputPositive net) g
+	pure NetOutputTensor
+		{ notPriors = f
+		, notValuation = reshape h
+		, notScalars = i
+		}
 
+type NurseSvetaNetSpecDef = NurseSvetaNetSpec 30 64 1
 type NurseSvetaNetDef = NurseSvetaNet 30 64 1
 
-netDefForward ::
-	( BatchNormDTypeIsValid dev 'Double
-	, KnownNat n, KnownNat (PerCell n)
-	, 1 <= PerCell n
-	) =>
+netDefForwardTT ::
+	BatchNormDTypeIsValid dev 'Double =>
 	NurseSvetaNetDef dev ->
 	Bool ->
-	Tensor dev 'Double [n, 7, 8, 16] ->
-	Tensor dev 'Double [n, 6] ->
-	IO (Tensor dev 'Double [n, 4, 8, 16], Tensor dev 'Double [n, 4])
-netDefForward = netForward
+	NetInputTensor dev ->
+	IO (NetOutputTensor dev)
+netDefForwardTT = netForward
+
+netDefForwardST ::
+	BatchNormDTypeIsValid dev 'Double =>
+	NurseSvetaNetDef dev ->
+	Bool ->
+	[NetInputStruct] ->
+	IO (NetOutputTensor dev)
+netDefForwardST net training = nisToTensor >=> netDefForwardTT net training
+
+netDefForwardSS ::
+	BatchNormDTypeIsValid dev 'Double =>
+	NurseSvetaNetDef dev ->
+	Bool ->
+	[NetInputStruct] ->
+	IO [NetOutputStruct]
+netDefForwardSS net training niss =
+	notToStruct [(nisLookaheadL nis, nisLookaheadR nis) | nis <- niss]
+	<$> netDefForwardST net training niss
+
+data Dict c where Dict :: c => Dict c
+
+unsafeRefl :: forall a b. Dict (a ~ b)
+unsafeRefl = unsafeCoerce (Dict @(a ~ a))
+
+withDict :: Dict c -> (c => r) -> r
+withDict Dict r = r
+
+{-# Inline unsafeKnownNat #-}
+unsafeKnownNat :: Int -> Dict (KnownNat n)
+unsafeKnownNat n = case someNatVal (toInteger n) of
+	Just (SomeNat (p :: Proxy n')) -> unsafeCoerce (Dict @(KnownNat n'))
+
+knownMul :: forall m n. (KnownNat m, KnownNat n) => Dict (KnownNat (m*n))
+knownMul = unsafeKnownNat (natValI @m * natValI @n)
+
+-- cmpNat isn't available in some of the bases we support
+leNat :: forall m n. (KnownNat m, KnownNat n) => Maybe (Dict (m <= n))
+leNat = if natValI @m <= natValI @n
+	then Just unsafeRefl
+	else Nothing
