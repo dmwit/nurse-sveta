6035be89652eda3faa9cb29607c110aa6e2e5496
Daniel Wagner <dmwit@galois.com>
2023-03-19 10:51:11 -0400

    succeed in resetting state if there's no state to reset

--- bin/reset-state.hs
+++ bin/reset-state.hs
@@ -1,7 +1,13 @@
 module Main where
 
+import Control.Exception
 import System.Directory
 import System.Environment.XDG.BaseDir
+import System.IO.Error
 
 main :: IO ()
-main = getUserDataDir "nurse-sveta" >>= removeDirectoryRecursive
+main = do
+	dir <- getUserDataDir "nurse-sveta"
+	catch (removeDirectoryRecursive dir) $ \e -> if isDoesNotExistError e
+		then pure ()
+		else throwIO e
