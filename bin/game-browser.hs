885ff0c66e704dc096a17d567e5fe022389f1132
Daniel Wagner <me@dmwit.com>
2026-02-13 02:13:54 -0500

    Add hover indicator and pill preview

--- bin/game-browser.hs
+++ bin/game-browser.hs
@@ -1,7 +1,10 @@
 module Main where
 
+import GI.Cairo.Render.Connector (renderWithContext)
+import qualified GI.Cairo.Render as C
 import GI.Gtk hiding (Text)
 import Ms.Mendel hiding (get)
+import qualified Nurse.Sveta.Cairo as NSC
 import Nurse.Sveta.GameBrowser
 
 import qualified Data.Map as M
@@ -16,12 +19,17 @@ main = do
 		top <- new Box [#orientation := OrientationHorizontal]
 		boardView <- newPlayerStateView (uiCurrentPSM def)
 		boardWidget <- psvWidget boardView
+		boardOverlay <- new Overlay [#child := boardWidget]
+		hoverLayer <- new DrawingArea [#hexpand := True, #vexpand := True, #canTarget := False]
+		#addOverlay boardOverlay hoverLayer
 		treeView <- new ListBox [#activateOnSingleClick := True, #selectionMode := SelectionModeNone]
 		treeScroll <- new ScrolledWindow [#child := treeView, #minContentWidth := 550]
 		tools <- new Box [#orientation := OrientationVertical]
 		uiRef <- newIORef (def :: UIModel)
 		toolRef <- newIORef (Blue, Blue)
 		dragStartRef <- newIORef (Nothing :: Maybe (Double, Double, Position))
+		hoverCellRef <- newIORef (Nothing :: Maybe Position)
+		previewPillRef <- newIORef (Nothing :: Maybe Pill)
 
 		seedEntry <- new Entry [#placeholderText := "seed", #maxLength := 4]
 		levelEntry <- new Entry [#placeholderText := "level", #maxLength := 2, #inputPurpose := InputPurposeDigits]
@@ -44,11 +52,39 @@ main = do
 		    	ui <- readIORef uiRef
 		    	psvSet boardView (uiCurrentPSM ui)
 		    	renderTreeView treeView boardView uiRef
+		    	#queueDraw hoverLayer
+
+		drawingAreaSetDrawFunc hoverLayer . Just $ \_ ctx ww wh -> do
+			ui <- readIORef uiRef
+			mhover <- readIORef hoverCellRef
+			mpreview <- readIORef previewPillRef
+			drawOverlay ui ww wh ctx mhover mpreview
+
+		motion <- new EventControllerMotion []
+		on motion #motion \x y -> do
+			cell <- psvPointToBoardCell boardView x y
+			writeIORef hoverCellRef cell
+			#queueDraw hoverLayer
+		on motion #leave do
+			writeIORef hoverCellRef Nothing
+			#queueDraw hoverLayer
+		#addController boardWidget motion
 
 		drag <- new GestureDrag []
 		on drag #dragBegin \sx sy -> do
 			mcell <- psvPointToBoardCell boardView sx sy
 			writeIORef dragStartRef (fmap (\cell -> (sx, sy, cell)) mcell)
+			writeIORef previewPillRef Nothing
+			#queueDraw hoverLayer
+		on drag #dragUpdate \dx dy -> do
+			ms <- readIORef dragStartRef
+			case ms of
+				Nothing -> pure ()
+				Just (sx0, sy0, Position sx sy) -> do
+					mend <- psvPointToBoardCell boardView (sx0 + dx) (sy0 + dy)
+					(c1, c2) <- readIORef toolRef
+					writeIORef previewPillRef (mend >>= \(Position ex ey) -> dragToPill c1 c2 (sx, sy) (ex, ey))
+					#queueDraw hoverLayer
 		on drag #dragEnd \dx dy -> do
 			ms <- readIORef dragStartRef
 			case ms of
@@ -62,6 +98,8 @@ main = do
 							modifyIORef uiRef \u -> fromMaybe u (uiTryAdvance (Lock pill) u)
 							refresh
 			writeIORef dragStartRef Nothing
+			writeIORef previewPillRef Nothing
+			#queueDraw hoverLayer
 		#addController boardWidget drag
 
 		seedBuffer <- get seedEntry #buffer
@@ -84,7 +122,7 @@ main = do
 		#append tools levelEntry
 		#append tools generateButton
 
-		#append top boardWidget
+		#append top boardOverlay
 		#append top treeScroll
 		#append top tools
 
@@ -142,6 +180,53 @@ dragToPill c1 c2 (sx, sy) (ex, ey) = case (ex - sx, ey - sy) of
 			}
 		, bottomLeftPosition = Position x y
 		}
+
+drawOverlay ui ww wh ctx mhover mpill = flip renderWithContext ctx do
+	case mpill of
+		Just preview -> do
+			let (gx, gy, gw, gh) = gridGeometry
+			C.save
+			C.translate gx gy
+			NSC.initMath (round gw) (round gh) (fromIntegral bw) (fromIntegral bh)
+			C.pushGroup
+			NSC.pill preview
+			C.popGroupToSource
+			C.paintWithAlpha 0.6
+			C.restore
+		Nothing -> for_ mhover \pos -> do
+			for_ (cellRect pos) \(x, y, cw, ch) -> do
+				C.setLineWidth 2
+				C.setSourceRGBA 1 1 1 0.9
+				C.rectangle x y cw ch
+				C.strokePreserve
+				C.setSourceRGBA 1 1 1 (0.9 * 0.15)
+				C.fill
+	where
+	cellRect (Position cx cy) =
+		let (gx, gy, gw, gh) = gridGeometry
+		    cw = gw / fromIntegral bw
+		    ch = gh / fromIntegral bh
+		    x0 = gx + cw * fromIntegral (cx + 1)
+		    y0 = gy + gh * (1 - fromIntegral (cy + 2) / fromIntegral bh)
+		in [(x0, y0, cw, ch)]
+
+	gridGeometry =
+		let ww' = fromIntegral ww
+		    wh' = fromIntegral wh
+		    aspect = fromIntegral bw / fromIntegral bh
+		in if ww' / wh' > aspect
+			then
+				let drawW = wh' * aspect
+				    offX = (ww' - drawW) / 2
+				in (offX, 0, drawW, wh')
+			else
+				let drawH = ww' / aspect
+				    offY = (wh' - drawH) / 2
+				in (0, offY, ww', drawH)
+
+	b = board (uiCurrentState ui)
+	bw = width b + 2
+	bh = height b + 4
 
 data TreeRow = TreeRow
 	{ trAddress :: MoveTreeAddress
